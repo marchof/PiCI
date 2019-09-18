82cee6142d5adbcb1d8a0c4227d8817024d321bc
Marc R. Hoffmann <hoffmann@mountainminds.com>
2019-09-18 21:00:36 +0100

    Move Dockerfiles to subfolder to avoid .dockerignore

--- jdk/build.sh
+++ jdk/build.sh
@@ -4,5 +4,5 @@ hg pull   --repository workspace
 hg update --repository workspace
 hg heads  --repository workspace
 
-docker build -t jdkbuild .
+docker build -t jdkbuild ./docker/
 docker run -t -i -v $(realpath ./workspace):/workspace jdkbuild
--- jdk12/build.sh
+++ jdk12/build.sh
@@ -4,5 +4,5 @@ hg pull   --repository workspace
 hg update --repository workspace
 hg heads  --repository workspace
 
-docker build -t jdk12build .
+docker build -t jdk12build ./docker/
 docker run -t -i -v $(realpath ./workspace):/workspace jdk12build
