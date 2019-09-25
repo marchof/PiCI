aa9ab8e8c3f386fae4d1a286b62259c7d469b921
Marc R. Hoffmann <hoffmann@mountainminds.com>
2019-09-25 21:02:43 +0100

    WIP: Verify JDKs with JaCoCo

---
+++ jacoco/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+REPO_URL="https://github.com/jacoco/jacoco.git"
+
+git -C workspace pull || git clone ${REPO_URL} workspace
+
+REVISION=$(git -C workspace rev-parse HEAD)
+
+ARTIFACTS="artifacts/${REVISION}"
+
+if [ ! -d "${ARTIFACTS}" ]; then
+
+  mkdir -p "${ARTIFACTS}"
+  ln -sf "${REVISION}" artifacts/latest
+
+  docker build -t jacocobuild ./docker/
+  docker run -t -i -v $(realpath ./workspace):/workspace -v $(realpath ../jdk11u/artifacts/lastSuccessful/jdk):/jdk -v $(realpath ${ARTIFACTS}):/artifacts jacocobuild
+
+  ln -sf "${REVISION}" artifacts/lastSuccessful
+
+fi
