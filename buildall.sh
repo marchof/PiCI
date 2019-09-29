850215ee901c7b17ef8dfed35a1ed9238c38992d
Marc R. Hoffmann <hoffmann@mountainminds.com>
2019-09-29 16:55:56 +0100

    Run build periodically

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e -o pipefail
 
-BUILDS="jdk11u jdk12u jdk13 jdk"
+BUILDS="jdk11u jdk12u jdk13 jdk jacoco"
 
 for build in $BUILDS; do
   (cd $build && ./build.sh)
---
+++ buildloop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e -o pipefail
+
+while :
+do
+
+  ./buildall.sh
+  sleep 30m
+
+done
