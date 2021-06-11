c237ae47dd06191aba9af571687a5980f256ee12
Marc R. Hoffmann <hoffmann@mountainminds.com>
2021-06-11 13:52:05 +0200

    Add jdk17 builds

---
+++ jdk17-jacoco/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e -o pipefail
+
+. $(dirname $0)/../shared/protobuild.sh
+
+doFetchInput() {
+  fetchGitRepo "https://github.com/jacoco/jacoco.git" "master"
+}
+
+doGetInputInfo() {
+  getGitInputInfo
+  cat ../jdk17/output/lastSuccessful/INPUT || true
+}
+
+doRunBuild() {
+  docker build -t jacocobuild ./docker/ &&
+  docker run -t -i -v ${WORKSPACE_DIR}:/workspace \
+                   -v $(realpath ../jdk17/output/lastSuccessful/artifacts/jdk):/jdk \
+                   -v m2repo:/m2repo \
+                   -v ${ARTIFACTS_DIR}:/artifacts jacocobuild
+}
+
+run "@$"
