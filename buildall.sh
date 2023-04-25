6ac23d61cc00002266accfd0b8b0db7384e75a11
Marc R. Hoffmann <hoffmann@mountainminds.com>
2023-04-25 18:55:03 +0200

    Add Java 20 build

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
 
-BUILDS="jdk11 jdk12 jdk13 jdk14 jdk15 jdk16 jdk17 jdk18 jdk19 jdk jdk11-jacoco jdk12-jacoco jdk13-jacoco jdk14-jacoco jdk15-jacoco jdk16-jacoco jdk17-jacoco jdk18-jacoco jdk19-jacoco jdk-jacoco"
+BUILDS="jdk11 jdk12 jdk13 jdk14 jdk15 jdk16 jdk17 jdk18 jdk19 jdk20 jdk jdk11-jacoco jdk12-jacoco jdk13-jacoco jdk14-jacoco jdk15-jacoco jdk16-jacoco jdk17-jacoco jdk18-jacoco jdk19-jacoco jdk20-jacoco jdk-jacoco"
 
 for build in $BUILDS; do
   (cd $build && ./build.sh)
---
+++ jdk20/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -o pipefail
+
+. $(dirname $0)/../shared/protobuild.sh
+
+doFetchInput() {
+  fetchGitRepo "https://github.com/openjdk/jdk20.git" "master"
+}
+
+doGetInputInfo() {
+  getGitInputInfo
+}
+
+doRunBuild() {
+  docker build -t jdkbuild ./docker/ &&
+  docker run -t -i -v ${WORKSPACE_DIR}:/workspace \
+                   -v ${ARTIFACTS_DIR}:/artifacts jdkbuild
+}
+
+run "@$"
