3651c22b6ac0fe7e7e39ba8c22b3d507d0d0dcd9
Marc R. Hoffmann <hoffmann@mountainminds.com>
2023-06-08 20:37:57 +0200

    Use update repos for Java 19 and 20

--- jdk20/build.sh
+++ jdk20/build.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 . $(dirname $0)/../shared/protobuild.sh
 
 doFetchInput() {
-  fetchGitRepo "https://github.com/openjdk/jdk20.git" "master"
+  fetchGitRepo "https://github.com/openjdk/jdk20u.git" "master"
 }
 
 doGetInputInfo() {
