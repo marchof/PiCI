e616ac13326ee5e00847ef26c9b6c4e858deac89
Marc R. Hoffmann <hoffmann@mountainminds.com>
2024-10-13 14:42:04 +0200

    Add JaCoCo builds with JDK 22 and 23

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
 
-BUILDS="jdk11 jdk17 jdk21 jdk22 jdk23 jdk jdk11-jacoco jdk17-jacoco jdk21-jacoco jdk-jacoco"
+BUILDS="jdk11 jdk17 jdk21 jdk22 jdk23 jdk jdk11-jacoco jdk17-jacoco jdk21-jacoco jdk22-jacoco jdk23-jacoco jdk-jacoco"
 
 for build in $BUILDS; do
   (cd $build && ./build.sh)
