ef60e28db866d79aef19345546c6d299bc4d24ba
Marc R. Hoffmann <hoffmann@mountainminds.com>
2025-08-04 14:30:44 +0200

    Add builds for Java 25

---
+++ jdk25/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e -o pipefail
+
+. $(dirname $0)/../shared/protobuild.sh
+
+doFetchInput() {
+  fetchGitRepo "https://github.com/openjdk/jdk25u.git" "master"
+}
+
+doGetInputInfo() {
+  getGitInputInfo
+}
+
+doRunBuild() {
+  docker build -t jdkbuild ./docker/ &&
+  docker run -t -i -v ${WORKSPACE_DIR}:/workspace \
+                   -v $(realpath ../jdk24/output/lastSuccessful/artifacts/jdk):/jdk \
+                   -v ${ARTIFACTS_DIR}:/artifacts jdkbuild
+}
+
+run "@$"
