debcbf93f8b072620b178836651767fea5fadab8
Marc R. Hoffmann <hoffmann@mountainminds.com>
2026-07-14 07:59:37 +0200

    Add build for Java 27

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
 
-BUILDS="jdk11 jdk17 jdk21 jdk22 jdk23 jdk24 jdk25 jdk26 jdk jdk11-jacoco jdk17-jacoco jdk21-jacoco jdk25-jacoco jdk26-jacoco jdk-jacoco"
+BUILDS="jdk11 jdk17 jdk21 jdk22 jdk23 jdk24 jdk25 jdk26 jdk27 jdk jdk11-jacoco jdk17-jacoco jdk21-jacoco jdk25-jacoco jdk26-jacoco jdk-jacoco"
 
 for build in $BUILDS; do
   (cd $build && ./build.sh)
--- jdk27/build.sh
+++ jdk27/build.sh
@@ -4,7 +4,7 @@ set -e -o pipefail
 . $(dirname $0)/../shared/protobuild.sh
 
 doFetchInput() {
-  fetchGitRepo "https://github.com/openjdk/jdk.git" "master"
+  fetchGitRepo "https://github.com/openjdk/jdk27u.git" "master"
 }
 
 doGetInputInfo() {
@@ -14,7 +14,7 @@ doGetInputInfo() {
 doRunBuild() {
   docker build -t jdkbuild ./docker/ &&
   docker run -t -i -v ${WORKSPACE_DIR}:/workspace \
-                   -v $(realpath ../jdk27/output/lastSuccessful/artifacts/jdk):/jdk \
+                   -v $(realpath ../jdk26/output/lastSuccessful/artifacts/jdk):/jdk \
                    -v ${ARTIFACTS_DIR}:/artifacts jdkbuild
 }
 
